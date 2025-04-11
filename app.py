8188548e04c7cc2b1de988d004dd0f34fce0579e
Nushrat Jahan Ria <njria99@gmail.com>
2025-04-11 13:55:16 -0500

    remove unnecessary .DS_Store file

--- app.py
+++ app.py
@@ -2,9 +2,20 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# @app.route("/")
+# def index():
+#     return render_template("index.html")
+
 @app.route("/")
-def index():
+def landing():
     return render_template("index.html")
+# @app.route("/")
+# def index():
+#     return render_template("index.html", background_image="background.jpg")
+
+@app.route("/task")
+def page2():
+    return render_template("sort.html", background_image="background2.jpg")  # Use your next page's image name here
 
 if __name__ == "__main__":
     app.run(debug=True)
