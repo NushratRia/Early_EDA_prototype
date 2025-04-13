ebfabf37c79c632308f65c3b4f5fc6b1f5b6837e
Nushrat Jahan Ria <njria99@gmail.com>
2025-04-13 14:51:16 -0500

    apr13

--- app.py
+++ app.py
@@ -15,7 +15,39 @@ def landing():
 
 @app.route("/task")
 def page2():
-    return render_template("sort.html", background_image="background2.jpg")  # Use your next page's image name here
+    return render_template("merge.html", background_image="background2.jpg")  # Use your next page's image name here
+
+@app.route('/merge2')
+def merge2():
+    return render_template('merge2.html') 
+
+@app.route('/sort')
+def sort():
+    return render_template('sort.html') 
+
+@app.route('/sort2')
+def sort2():
+    return render_template('sort2.html') 
+
+@app.route('/remove')
+def remove():
+    return render_template('remove.html') 
+
+@app.route('/remove2')
+def remove2():
+    return render_template('remove2.html') 
+
+@app.route('/missing')
+def missing():
+    return render_template('missing.html') 
+
+@app.route('/missing2')
+def missing2():
+    return render_template('missing2.html') 
+
+@app.route('/thanks')
+def thanks():
+    return render_template('thanks.html') 
 
 if __name__ == "__main__":
     app.run(debug=True)
